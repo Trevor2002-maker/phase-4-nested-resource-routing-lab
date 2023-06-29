c60156d702f509113a0e1445745ab368e4c212e3
trevormadaramoringaschool <trevor.madara@student.moringaschool.gcom>
2023-06-29 10:18:00 +0300

    commit tree 03665acb656bf55b81a531c5e890b30afafc5816

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,39 @@
 class ItemsController < ApplicationController
+  before_action :set_user
 
   def index
-    items = Item.all
-    render json: items, include: :user
+    @items = @user.items.includes(:user)
+    render json: @items, include: :user
   end
 
+  def show
+    @item = @user.items.find_by(id: params[:id])
+    if @item
+      render json: @item, include: :user
+    else
+      render json: { error: 'Item not found' }, status: :not_found
+    end
+  end
+
+  def create
+    @item = @user.items.build(item_params)
+    if @item.save
+      render json: @item, status: :created
+    else
+      render json: @item.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find_by(id: params[:user_id])
+    unless @user
+      render json: { error: 'User not found' }, status: :not_found
+    end
+  end
+
+  def item_params
+    params.require(:item).permit(:name, :description, :price)
+  end
 end
